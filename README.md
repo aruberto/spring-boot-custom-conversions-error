cc11b044ef4caff45ddf590266a5e666b1a97ddd
Antonio Ruberto <aruberto@realmatters.com>
2017-02-07 16:00:24 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# spring-boot-custom-conversions-error 
---
+++ src/main/java/io/github/aruberto/CustomConversionsBugApplication.java
@@ -0,0 +1,12 @@
+package io.github.aruberto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomConversionsBugApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomConversionsBugApplication.class, args);
+	}
+}
